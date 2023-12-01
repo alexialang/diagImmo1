ba974aaaa462d44884930c8e57af013774a306b3
Adrien Faletic <adrien.faletic@gmail.com>
2023-12-01 17:01:44 +0100

    Update styles in Devis and AvtAchatLoc modules

--- src/pages/AvtAchatLoc/Dpe.jsx
+++ src/pages/AvtAchatLoc/Dpe.jsx
@@ -3,49 +3,78 @@ import { Button } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 export const Dpe = () => {
-  return(
-  <div>
-    <div className={styles.backgroundImg}>
-      <h1>Dpe</h1><div className={styles.block}>
-      <p>Dpe</p></div> 
-      </div>
-    <div className={styles.txt} >
-    <p>Bienvenue chez  GPADI, votre partenaire de confiance pour tous vos besoins en diagnostics immobiliers. Nous comprenons l’importance de garantir la transparence et la conformité dans le domaine de la localisation immobilière, c’est pourquoi nous mettons en avant notre expertise, cette fois-ci, en matière de Diagnostic de Performance Énergétique (DPE).
-    </p>
-<h2>Qu’est-ce que le DPE ?</h2>
-<p>Le Diagnostic de Performance Énergétique (DPE) est une étape cruciale lors de la mise en vente ou en location d’un bien immobilier. Il vise à évaluer la performance énergétique du logement en attribut des notes de consommation énergétique et d’émissions de gaz à effet de serre. Notre équipe chez [Nom de votre entreprise] est prête à vous accompagner à travers ce processus essentiel.</p>
-
-<h2> Obligations et dates importantes</h2>
-<h3>· Obligation Légale : </h3><p>Le DPE est une obligation légale lors de la vente ou de la location d’un bien immobilier en France. Il doit être inclus dans les annonces immobilières pour informer les futurs occupants.</p>
-
-<h3>· Validité du DPE :</h3> La validité du DPE est de 10 ans, sauf en cas de travaux significatifs impactant la performance énergétique du logement. Dans ce cas, une mise à jour du diagnostic est recommandée.
-
-<h2>Notre Engagement envers la Conformité</h2>
-Chez  GPADI, nous nous engageons à assurer une totale conformité aux exigences du DPE. Nos diagnostiqueurs expérimentés sont formés pour fournir des évaluations précises de la performance énergétique de votre logement, vous aidant à prendre des décisions éclairées.
-
-<h2>Pourquoi nous choisir pour votre Diagnostic DPE ?</h2>
-<p> · Expertise Approfondie : Nos diagnostiqueurs sont certifiés et experts dans la réalisation de diagnostics DPE conformes aux normes en vigueur.
-
-<br/>· Conseils Personnalisés : Besoin de conseils sur les démarches à suivre en fonction des résultats du DPE ? Notre équipe est là pour vous fournir les informations nécessaires.
-
-<br/>· Minimisation des Risques : En concevant  GPADI, minimisez les risques liés à la performance énergétique de votre logement, contribuant ainsi à la durabilité et à l’efficacité énergétique.</p>
-
-<p className={styles.txtColor}>Besoin d’un DEVIS pour votre diagnostic immobilier DPE ? Contactez-nous dès Aujourd’hui!</p>
+  return (
     <div>
-    <Link to="/devis">
-          <Button
-            className={styles.buttonFirstImg}
-            variant="filled"
-            color="yellow"
-            size="l"
-            radius="xl"
-          >
-            Cliquez ici
-          </Button>
-        </Link></div>
+      <div className={styles.backgroundImg}>
+        <h1>Dpe</h1>
+        <div className={styles.block}>
+          <p>Dpe</p>
+        </div>
+      </div>
+      <div className={styles.txt}>
+        <p>
+          Bienvenue chez GPADI, votre partenaire de confiance pour tous vos
+          besoins en diagnostics immobiliers. Nous comprenons l’importance de
+          garantir la transparence et la conformité dans le domaine de la
+          localisation immobilière, c’est pourquoi nous mettons en avant notre
+          expertise, cette fois-ci, en matière de Diagnostic de Performance
+          Énergétique (DPE).
+        </p>
+        <h2>Qu’est-ce que le DPE ?</h2>
+        <p>
+          Le Diagnostic de Performance Énergétique (DPE) est une étape cruciale
+          lors de la mise en vente ou en location d’un bien immobilier. Il vise
+          à évaluer la performance énergétique du logement en attribut des notes
+          de consommation énergétique et d’émissions de gaz à effet de serre.
+          Notre équipe chez [Nom de votre entreprise] est prête à vous
+          accompagner à travers ce processus essentiel.
+        </p>
+        <h2> Obligations et dates importantes</h2>
+        <h3>· Obligation Légale : </h3>
+        <p>
+          Le DPE est une obligation légale lors de la vente ou de la location
+          d’un bien immobilier en France. Il doit être inclus dans les annonces
+          immobilières pour informer les futurs occupants.
+        </p>
+        <h3>· Validité du DPE :</h3> La validité du DPE est de 10 ans, sauf en
+        cas de travaux significatifs impactant la performance énergétique du
+        logement. Dans ce cas, une mise à jour du diagnostic est recommandée.
+        <h2>Notre Engagement envers la Conformité</h2>
+        Chez GPADI, nous nous engageons à assurer une totale conformité aux
+        exigences du DPE. Nos diagnostiqueurs expérimentés sont formés pour
+        fournir des évaluations précises de la performance énergétique de votre
+        logement, vous aidant à prendre des décisions éclairées.
+        <h2>Pourquoi nous choisir pour votre Diagnostic DPE ?</h2>
+        <p>
+          {" "}
+          · Expertise Approfondie : Nos diagnostiqueurs sont certifiés et
+          experts dans la réalisation de diagnostics DPE conformes aux normes en
+          vigueur.
+          <br />· Conseils Personnalisés : Besoin de conseils sur les démarches
+          à suivre en fonction des résultats du DPE ? Notre équipe est là pour
+          vous fournir les informations nécessaires.
+          <br />· Minimisation des Risques : En concevant GPADI, minimisez les
+          risques liés à la performance énergétique de votre logement,
+          contribuant ainsi à la durabilité et à l’efficacité énergétique.
+        </p>
+        <p className={styles.txtColor}>
+          Besoin d’un DEVIS pour votre diagnostic immobilier DPE ?
+          Contactez-nous dès Aujourd’hui!
+        </p>
+        <div>
+          <Link to="/devis">
+            <Button
+              className={styles.buttonFirstImg}
+              variant="filled"
+              color="yellow"
+              size="l"
+              radius="xl"
+            >
+              Cliquez ici
+            </Button>
+          </Link>
+        </div>
+      </div>
     </div>
-   
-        </div> )
-}
-  
-    
+  );
+};
--- src/pages/Devis/Devis.jsx
+++ src/pages/Devis/Devis.jsx
@@ -1,7 +1,232 @@
 import styles from "./devis.module.css";
+import { useForm } from "@mantine/form";
+import {
+  TextInput,
+  Checkbox,
+  Button,
+  Group,
+  Box,
+  Select,
+  Radio,
+} from "@mantine/core";
 
 export const Devis = () => {
+  const form = useForm({
+    initialValues: {
+      type: "",
+      wish: "",
+      electric15: "",
+      gaz15: "",
+      rooms: "",
+      surface: "",
+      constructionDate: "",
+      heating: "",
+      gazType: "",
+      coOwnership: "",
+      adress: "",
+      postalCode: "",
+      city: "",
+      moreInfo: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+    },
+  });
+
   return (
-    <div className={styles.imgPano}></div>
-  )
-}
+    <div className={styles.root}>
+      <div className={styles.imgPano}></div>
+      <div
+        style={{
+          marginTop: "60px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          rowGap: "20px",
+        }}
+      >
+        <div>Votre bien</div>
+        <div>
+          <Box mx="auto">
+            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+              <Select
+                label="Type de bien..."
+                placeholder="Type de bien..."
+                data={[
+                  "Un appartement",
+                  "Une maison",
+                  "Un immeuble",
+                  "Un local conmercial",
+                  "Des parties communes",
+                  "Un terrain",
+                  "Une cave",
+                  "Un garage",
+                  "Autre",
+                ]}
+                {...form.getInputProps("type")}
+              />
+              <Radio.Group
+                label="Vous souhaitez"
+                withAsterisk
+                {...form.getInputProps("wish")}
+              >
+                <Group>
+                  <Radio label="Vendre" value="sell" />
+                  <Radio label="Louer" value="rent" />
+                </Group>
+              </Radio.Group>
+              <Radio.Group
+                label="Installation elec +15ans"
+                withAsterisk
+                {...form.getInputProps("electric15")}
+              >
+                <Group>
+                  <Radio label="Oui" value="yes" />
+                  <Radio label="Non" value="no" />
+                </Group>
+              </Radio.Group>
+              <Select
+                withAsterisk
+                label="Nombre de pièces..."
+                placeholder="Nombre de pièces..."
+                data={[
+                  "Chambre (< 12m²)",
+                  "Studio",
+                  "2 pièces",
+                  "3 pièces",
+                  "4 pièces",
+                  "5 pièces",
+                  "6 pièces",
+                  "7 pièces",
+                  "8 pièces",
+                  "9 pièces",
+                  "10 pièces",
+                ]}
+                {...form.getInputProps("rooms")}
+              />
+              <Radio.Group
+                label="Installation gaz +15ans"
+                withAsterisk
+                {...form.getInputProps("gaz15")}
+              >
+                <Group>
+                  <Radio label="Oui" value="yes" />
+                  <Radio label="Non" value="no" />
+                </Group>
+              </Radio.Group>
+              <TextInput
+                withAsterisk
+                label="Surface habitable m²"
+                {...form.getInputProps("surface")}
+              />
+              <Select
+                withAsterisk
+                label="Date de construction"
+                placeholder="Date de construction"
+                data={[
+                  "Avant Janvier 1949",
+                  "Avant Juillet 1997",
+                  "Après Juillet 1997",
+                  "Après Juillet 1997 mais plus de 15 ans",
+                  "Je ne sais pas",
+                ]}
+                {...form.getInputProps("constructionDate")}
+              />
+              <Select
+                withAsterisk
+                label="Chauffage"
+                placeholder="Chauffage"
+                data={[
+                  "Gaz",
+                  "Electrique",
+                  "Bois",
+                  "Fioul",
+                  "Autre",
+                  "Je ne sais pas",
+                ]}
+                {...form.getInputProps("heating")}
+              />
+              <Select
+                withAsterisk
+                label="Gaz"
+                placeholder="Gaz"
+                data={[
+                  "Aucune installation",
+                  "Gaz de ville",
+                  "Gaz en citerne",
+                  "Gaz en bouteille",
+                  "Présence d'un compteur gaz sans abonement",
+                  "Autre",
+                  "Je ne sais pas",
+                ]}
+                {...form.getInputProps("gazType")}
+              />
+              <Radio.Group
+                label="Copropriété"
+                withAsterisk
+                {...form.getInputProps("coOwnership")}
+              >
+                <Group>
+                  <Radio label="Oui" value="yes" />
+                  <Radio label="Non" value="no" />
+                  <Radio label="Je ne sais pas" value="dontKnow" />
+                </Group>
+              </Radio.Group>
+              <TextInput
+                withAsterisk
+                label="Adresse"
+                autoComplete="street-address"
+                {...form.getInputProps("adress")}
+              />
+              <TextInput
+                withAsterisk
+                label="Code postal"
+                autoComplete="postal-code"
+                {...form.getInputProps("postalCode")}
+              />
+              <TextInput
+                withAsterisk
+                label="Ville"
+                autoComplete="address-level2"
+                {...form.getInputProps("city")}
+              />
+              <TextInput
+                label="Informations complémentaires"
+                placeholder="Informations complémentaires"
+                {...form.getInputProps("moreInfo")}
+              />
+              <TextInput
+                withAsterisk
+                label="Prénom"
+                placeholder="Prénom"
+                autoComplete="given-name"
+                {...form.getInputProps("firstName")}
+              />
+              <TextInput
+                withAsterisk
+                label="Nom"
+                placeholder="Nom"
+                autoComplete="family-name"
+                {...form.getInputProps("lastName")}
+              />
+              <TextInput
+                withAsterisk
+                label="Email"
+                placeholder="your@email.com"
+                {...form.getInputProps("email")}
+              />
+              <Group justify="flex-end" mt="md">
+                <Button type="submit">Submit</Button>
+              </Group>
+            </form>
+          </Box>
+        </div>
+      </div>
+    </div>
+  );
+};
